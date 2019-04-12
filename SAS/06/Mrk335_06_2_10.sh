bc3dc58d83900cee5ec1dc8646e781c632db28ed
Ylllllax <31843279+Ylllllax@users.noreply.github.com>
2019-04-12 20:59:53 +0800

    Add files via upload

---
+++ SAS/06/Mrk335_06_2_10.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+heaini
+sasini
+
+export SAS_CCF=ccf.cif
+odf=$(echo $(ls *SUM.SAS))
+export SAS_ODF=$odf
+
+#源
+evselect table=EPICclean.fits energycolumn=PI \
+expression='#XMMEA_EP && (PATTERN<=4) && ((X,Y) IN circle(27770.5, 26940.5,1000)) &&(PI in [2000:10000])' \
+withrateset=yes rateset="PN_lc_source_raw_2_10.lc" timebinsize=200 \
+maketimecolumn=yes makeratecolumn=yes 
+
+#背景
+evselect table=EPICclean.fits \
+energycolumn=PI \
+expression='#XMMEA_EP && (PATTERN<=4) && ((X,Y) IN circle(27640.5,22580.5,1000))&&(PI in [2000:10000])' \
+withrateset=yes rateset="PN_lc_background_raw_2_10.lc" timebinsize=200 \
+maketimecolumn=yes makeratecolumn=yes 
+
+
+epiclccorr srctslist=PN_lc_source_raw_2_10.lc eventlist=EPICclean.fits outset=PN_lccorr_2_10.lc \
+bkgtslist=PN_lc_background_raw_2_10.lc withbkgset=yes applyabsolutecorrections=yes 
+
+#fv PN_lccorr_2_10.lc
